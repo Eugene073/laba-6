840dba7e45ebcce78e2d163340a422924f730c37
Eugene073 <124811025+Eugene073@users.noreply.github.com>
2023-05-25 19:55:32 +0400

    Add files via upload

---
+++ laba-6p2.2.py
@@ -0,0 +1,47 @@
+"""В меню кафе 10 разных типов десертов. Сформировать (вывести) все возможные варианты десертов для 3 девушек."""
+desserts = [{'name': 'десерт №1', 'calories': 200},
+            {'name': 'десерт №2', 'calories': 300},
+            {'name': 'десерт №3', 'calories': 150},
+            {'name': 'десерт №4', 'calories': 250},
+            {'name': 'десерт №5', 'calories': 350},
+            {'name': 'десерт №6', 'calories': 180},
+            {'name': 'десерт №7', 'calories': 280},
+            {'name': 'десерт №8', 'calories': 220},
+            {'name': 'десерт №9', 'calories': 320},
+            {'name': 'десерт №10', 'calories': 120}]
+
+# создание списка девушек
+girls = ['1-ая девушка', '2-ая девушка', '3-ая девушка']
+
+# создание списка всех возможных комбинаций десертов
+combinations = []
+for i in range(len(desserts)):
+    for j in range(len(desserts)):
+        for k in range(len(desserts)):
+            combination = [desserts[i], desserts[j], desserts[k]]
+            if combination not in combinations:
+                combinations.append(combination)
+
+# ограничение на калорийность
+mcol = 720
+filtered_combinations = [combination for combination in combinations if sum(dessert['calories'] for dessert in combination) <= mcol]
+
+# целевая функция - минимизация калорийности
+def calorie_count(combination):
+    count = 0
+    for dessert in combination:
+        count += dessert['calories']
+    return count
+
+# сортировка по целевой функции
+filtered_combinations = sorted(filtered_combinations, key=calorie_count)
+
+# вывод результата
+count = len(filtered_combinations)
+for i, combination in enumerate(filtered_combinations):
+    print("Вариант №", i+1, "- калорийность:", calorie_count(combination))
+    for girl, dessert in zip(girls, combination):
+        print(girl, 'будет есть', dessert['name'], '({} ккал)'.format(dessert['calories']))
+    print('-' * 20)
+
+print("Итоговое количество вариантов:", count)
